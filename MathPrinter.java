45f5c933851ec7f838f0fe7035bb92a951eee785
JohnnyWu123 <39504017+JohnnyWu123@users.noreply.github.com>
2018-05-21 17:39:13 -0700

    Update MathPrinter.java

--- MathPrinter.java
+++ MathPrinter.java
@@ -3,10 +3,9 @@
  * 
  * @author me
  */
-public class MathPrinter
+public class HellowWorld
 {
    public static void main(String[] args){
-       System.out.println(1 + 3 + 5 + 7 + 9);
-       System.out.println(1 * 3 * 5 * 7 * 9);
+       System.out.println("Hello World");
    }
 }
