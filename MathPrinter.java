596165cee6aad59bef594e951b5374c79bb606a0
JohnnyWu123 <39504017+JohnnyWu123@users.noreply.github.com>
2018-05-21 17:28:34 -0700

    Add files via upload

---
+++ MathPrinter.java
@@ -0,0 +1,12 @@
+/**
+ * Do some math in the println statement.
+ * 
+ * @author Kathlen O'Brien
+ */
+public class MathPrinter
+{
+   public static void main(String[] args){
+       System.out.println(1 + 3 + 5 + 7 + 9);
+       System.out.println(1 * 3 * 5 * 7 * 9);
+   }
+}
